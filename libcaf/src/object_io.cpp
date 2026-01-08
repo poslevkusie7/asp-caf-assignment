68ceeac3ff60c259145c02509c9c8bda3e9f3bc0
Ido Ben-Yair <ido.benyair@gmail.com>
2026-01-08 12:18:09 +0200

    Fixed Tree objects not created in canonical form (intentionally broken test for the 2026 class)

--- libcaf/src/object_io.cpp
+++ libcaf/src/object_io.cpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <cstring>
 #include <stdexcept>
-#include <unordered_map>
+#include <map>
 
 #include "caf.h"
 #include "object_io.h"
@@ -99,7 +99,7 @@ Tree load_tree(const std::string &root_dir, const std::string &tree_hash) {
     if (read(fd, &num_records, sizeof(num_records)) != sizeof(num_records))
         throw std::runtime_error("Failed to read the number of records");
 
-    std::unordered_map<std::string, TreeRecord> records;
+    std::map<std::string, TreeRecord> records;
     for (uint32_t i = 0; i < num_records; ++i) {
         TreeRecord record = load_tree_record(fd);
         records.emplace(record.name, record);
